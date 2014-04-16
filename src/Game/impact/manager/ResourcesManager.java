c2bac8a9c24e485ecb0321c6e1315c61c5d4fd0f
James Reinholdt <jjreinholdt@gmail.com>
2014-04-15 21:52:40 -0500

    Implemented Random generation with progression

---
+++ src/Game/impact/manager/ResourcesManager.java
@@ -0,0 +1,211 @@
+package Game.impact.manager;
+
+import org.andengine.audio.music.Music;
+import org.andengine.audio.music.MusicFactory;
+import org.andengine.audio.sound.Sound;
+import org.andengine.engine.Engine;
+import org.andengine.engine.camera.BoundCamera;
+import org.andengine.opengl.font.Font;
+import org.andengine.opengl.font.FontFactory;
+import org.andengine.opengl.texture.ITexture;
+import org.andengine.opengl.texture.TextureOptions;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.andengine.opengl.texture.atlas.bitmap.BuildableBitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.source.IBitmapTextureAtlasSource;
+import org.andengine.opengl.texture.atlas.buildable.builder.BlackPawnTextureAtlasBuilder;
+import org.andengine.opengl.texture.atlas.buildable.builder.ITextureAtlasBuilder.TextureAtlasBuilderException;
+import org.andengine.opengl.texture.region.ITextureRegion;
+import org.andengine.opengl.texture.region.ITiledTextureRegion;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
+import org.andengine.util.debug.Debug;
+
+import android.graphics.Color;
+
+import Game.impact.GameActivity;
+
+import org.andengine.ui.IGameInterface.OnCreateResourcesCallback;
+import org.andengine.ui.activity.BaseGameActivity;
+
+public class ResourcesManager
+{
+	//---------------------------------------------
+	// VARIABLES
+	//---------------------------------------------
+	
+	private static final ResourcesManager INSTANCE = new ResourcesManager();
+	
+	public Engine engine;
+	public GameActivity activity;
+	public BoundCamera camera;
+	public VertexBufferObjectManager vbom;
+	
+	public Font font;
+	
+	//---------------------------------------------
+	// TEXTURES & TEXTURE REGIONS
+	//---------------------------------------------
+	
+	Sound mySound;
+	Music myMusic;
+	
+	public ITextureRegion splash_region;
+	public ITextureRegion menu_background_region;
+	public ITextureRegion play_region;
+	public ITextureRegion options_region;
+	
+	// Game Texture
+	public static BuildableBitmapTextureAtlas gameTextureAtlas;
+	
+	// Game Texture Regions
+	public static ITextureRegion bird_region;
+	public static ITextureRegion platform1_region;
+	public static ITextureRegion platform2_region;
+	public static ITextureRegion platform3_region;
+	public ITextureRegion coin_region;
+	public ITextureRegion complete_window_region;
+	public ITiledTextureRegion player_region;
+	public static ITiledTextureRegion explosion_region;
+	
+	private BitmapTextureAtlas splashTextureAtlas;
+	private BuildableBitmapTextureAtlas menuTextureAtlas;
+	
+	// Level Complete Window
+	public ITextureRegion rotator_region;
+	public ITiledTextureRegion complete_stars_region;
+	
+	//---------------------------------------------
+	// CLASS LOGIC
+	//---------------------------------------------
+
+	public void loadMenuResources()
+	{
+		loadMenuGraphics();
+		loadMenuAudio();
+		loadMenuFonts();
+	}
+	
+	public void loadGameResources()
+	{
+		loadGameGraphics();
+		loadGameFonts();
+		loadGameAudio();
+	}
+	
+	private void loadMenuGraphics()
+	{
+		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/menu/");
+        menuTextureAtlas = new BuildableBitmapTextureAtlas(activity.getTextureManager(), 680, 1200, TextureOptions.BILINEAR);
+        menu_background_region = BitmapTextureAtlasTextureRegionFactory.createFromAsset(menuTextureAtlas, activity, "menu_background.png");
+        play_region = BitmapTextureAtlasTextureRegionFactory.createFromAsset(menuTextureAtlas, activity, "play.png");
+        options_region = BitmapTextureAtlasTextureRegionFactory.createFromAsset(menuTextureAtlas, activity, "options.png");
+       
+    	try 
+    	{
+			this.menuTextureAtlas.build(new BlackPawnTextureAtlasBuilder<IBitmapTextureAtlasSource, BitmapTextureAtlas>(0, 1, 0));
+			this.menuTextureAtlas.load();
+		} 
+    	catch (final TextureAtlasBuilderException e)
+    	{
+			Debug.e(e);
+		}
+	}
+	
+	private void loadMenuAudio()
+	{
+		
+	}
+	
+	private void loadMenuFonts()
+	{
+		FontFactory.setAssetBasePath("font/");
+		final ITexture mainFontTexture = new BitmapTextureAtlas(activity.getTextureManager(), 256, 256, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+
+		font = FontFactory.createStrokeFromAsset(activity.getFontManager(), mainFontTexture, activity.getAssets(), "font.ttf", 50, true, Color.WHITE, 2, Color.BLACK);
+		font.load();
+	}
+
+	private void loadGameGraphics()
+	{
+		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/game/");
+        gameTextureAtlas = new BuildableBitmapTextureAtlas(activity.getTextureManager(), 1024, 1024, TextureOptions.BILINEAR);
+        
+       	platform1_region = BitmapTextureAtlasTextureRegionFactory.createFromAsset(gameTextureAtlas, activity, "platform1.png");
+       	platform2_region = BitmapTextureAtlasTextureRegionFactory.createFromAsset(gameTextureAtlas, activity, "platform2.png");
+       	platform3_region = BitmapTextureAtlasTextureRegionFactory.createFromAsset(gameTextureAtlas, activity, "platform3.png");
+       	bird_region = BitmapTextureAtlasTextureRegionFactory.createFromAsset(gameTextureAtlas, activity, "bird.png");
+        coin_region = BitmapTextureAtlasTextureRegionFactory.createFromAsset(gameTextureAtlas, activity, "coin.png");
+        complete_window_region = BitmapTextureAtlasTextureRegionFactory.createFromAsset(gameTextureAtlas, activity, "levelCompleteWindow.png");
+        rotator_region = BitmapTextureAtlasTextureRegionFactory.createFromAsset(gameTextureAtlas, activity, "rotator.png");
+ 
+        complete_stars_region = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(gameTextureAtlas, activity, "star.png", 2, 1);
+        player_region = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(gameTextureAtlas, activity, "player.png", 3, 1);
+        explosion_region = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(gameTextureAtlas, activity, "explosion.png", 4, 4);
+    	try 
+    	{
+			this.gameTextureAtlas.build(new BlackPawnTextureAtlasBuilder<IBitmapTextureAtlasSource, BitmapTextureAtlas>(0, 1, 0));
+			this.gameTextureAtlas.load();
+		} 
+    	catch (final TextureAtlasBuilderException e)
+    	{
+			Debug.e(e);
+		}
+	}
+	
+	private void loadGameFonts()
+	{
+		
+	}
+	
+	private void loadGameAudio()
+	{
+		
+	}
+	
+	public void unloadGameTextures()
+	{
+		// TODO (Since we did not create any textures for game scene yet)
+	}
+	
+	public void loadSplashScreen()
+	{
+		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
+        splashTextureAtlas = new BitmapTextureAtlas(activity.getTextureManager(), 1024, 500, TextureOptions.BILINEAR);
+        splash_region = BitmapTextureAtlasTextureRegionFactory.createFromAsset(splashTextureAtlas, activity, "Impact.png", 0, 0);
+        splashTextureAtlas.load();	
+	}
+	
+	public void unloadSplashScreen()
+	{
+		splashTextureAtlas.unload();
+		splash_region = null;
+	}
+	
+	public void unloadMenuTextures()
+	{
+		menuTextureAtlas.unload();
+	}
+	
+	public void loadMenuTextures()
+	{
+		menuTextureAtlas.load();
+	}
+	
+
+	public static void prepareManager(Engine engine, GameActivity activity, BoundCamera camera, VertexBufferObjectManager vbom)
+	{
+		getInstance().engine = engine;
+		getInstance().activity = activity;
+		getInstance().camera = camera;
+		getInstance().vbom = vbom;
+	}
+	
+	//---------------------------------------------
+	// GETTERS AND SETTERS
+	//---------------------------------------------
+	
+	public static ResourcesManager getInstance()
+	{
+		return INSTANCE;
+	}
+}
